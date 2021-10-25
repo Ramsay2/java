7dccd2ead029b3a36bf150b3cf3b312e5028517f
Ramsay2 <sachin114sharma@gmail.com>
2021-10-26 00:44:19 +0530

    next smaller of next greater

--- Eval25_10_2021/src/NSG_Product.java
+++ Eval25_10_2021/src/NSG_Product.java
@@ -1,19 +1,47 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class NSG_Product {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while (t-- != 0){
+        while (t-- != 0) {
             int n = sc.nextInt();
             int[] arr = new int[n];
-            for(int i = 0; i < n ; i++){
+            for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
             }
-            Stack<Integer> stk = new Stack<>();
+            nextMax(arr,n);
+        }
+    }
+
+    public static void nextMax(int[] arr,  int n) {
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            boolean flag = false;
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[j] > arr[i]) {
+                    ans *= nextMin(arr,j,n);
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) ans *= -1;
 
         }
+        System.out.println(ans);
+
+    }
+
+    public static int nextMin(int[] arr, int k, int n) {
+
+            for (int i = k + 1; i < n; i++) {
+                    if (arr[i] < arr[k]) {
+                        return  arr[i];
+                    }
+        }
+        return -1;
+
     }
 }
