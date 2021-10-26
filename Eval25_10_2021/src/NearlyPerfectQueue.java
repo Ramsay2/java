6d21013c60cffbbd98c5e557c3337e10040e06ba
Ramsay2 <sachin114sharma@gmail.com>
2021-10-26 23:42:29 +0530

    Commit

---
+++ Eval25_10_2021/src/NearlyPerfectQueue.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class NearlyPerfectQueue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+            findN(arr, m , 0, n - 1);
+        }
+
+    }
+
+    public static void findN(int[] arr, int m , int start, int end) {
+
+        boolean flag = false;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == m){
+                System.out.println(mid);
+                flag = true;
+                break;
+            }
+            if(arr[mid + 1] == m && mid < end){
+                System.out.println(mid + 1);
+                flag = true;
+                break;
+            }
+            if(arr[mid - 1] == m && mid > start){
+                System.out.println(mid - 1);
+                flag = true;
+                break;
+            }
+
+            if(arr[mid] > m) end = mid - 2;
+            else start = mid + 2;
+
+       /*     if(arr[mid] < arr[mid - 1]){
+                if(arr[mid - 1] < m){
+                    start = mid;
+                }else if(arr[mid - 1] > m){
+                    end = mid;
+                }else if(arr[mid - 1] == m){
+                    System.out.println(mid - 1);
+                    flag = true;
+                    break;
+                }
+            }else{
+                if(arr[mid] < m){
+                    start = mid + 1;
+                }else if(arr[mid] > m){
+                    end = mid - 1;
+                }else if(arr[mid] == m){
+                    System.out.println(mid);
+                    flag = true;
+                    break;
+                }
+
+            }*/
+
+
+        }
+        if(!flag){
+            System.out.println("Not Found");
+        }
+    }
+}
