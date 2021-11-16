81fd63fa3ae6c7684501e978fb0f14887de4e054
Ramsay2 <sachin114sharma@gmail.com>
2021-11-17 00:53:22 +0530

    Gifts

---
+++ DSAPractice1/src/Gifts.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Gifts {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+            System.out.println(gifts(arr, n));
+
+        }
+    }
+
+    static int gifts(int[] arr, int n) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for (int i = 0, j = 0; i < n; i++) {
+
+            j = Math.max(map.containsKey(arr[i]) ? map.get(arr[i]) : 0, j);
+            if (map.containsKey(arr[i])) {
+            j = Math.max(map.get(arr[i]),  j);
+            }
+            ans = Math.max(ans, i - j + 1);
+
+            map.put(arr[i], i + 1);
+        }
+
+        return ans;
+    }
+}
