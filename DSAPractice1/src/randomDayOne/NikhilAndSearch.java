e6004713de1682391442775bf710679a8c49153e
Ramsay2 <sachin114sharma@gmail.com>
2021-12-08 02:21:24 +0530

    commit

--- DSAPractice1/src/randomDayOne/NikhilAndSearch.java
+++ DSAPractice1/src/randomDayOne/NikhilAndSearch.java
@@ -1,10 +1,60 @@
 package randomDayOne;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class NikhilAndSearch {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        FastReader sc = new FastReader();
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
@@ -19,34 +69,43 @@ public class NikhilAndSearch {
     }
 
     private static int numbersInRange(int[] arr, int x, int y) {
+        Arrays.sort(arr);
+        int ans = -1;
+        int start = 0, end = arr.length - 1;
+        if (x == 0) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] == y) {
+                    ans = arr.length - mid;
+                    end = mid - 1;
+                }
+                if (arr[mid] < y) {
+                    start = mid + 1;
+                } else {
+                    ans = arr.length - mid;
+                    end = mid - 1;
+                }
+            }
+            if (ans == -1) {
+                return 0;
+            }
+            return ans;
 
-     int ans1 = -1;
-     int ans2 = -1;
-       int start = 0;
-       int end = arr.length - 1;
-       while (start <= end) {
-           int mid = (start + end) / 2;
-           if (arr[mid] == y) {
-               ans1 = mid;
-               break;
-           }
-           if (arr[mid] < y) {
-               start = mid + 1;
-           }
-           if (arr[mid] > y) {
-               ans2 = mid;
-               end = mid - 1;
-           }
+        } else if (x == 1) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
 
-       }
-       if(x == 0 && ans1 != -1) {
-           return arr.length - ans1;
-       }else if(x == 0) {
-           return arr.length - ans2;
-       }else   if(ans1 != -1) {
-           return 0;
-       }
-
-        return ans1;
+                if (arr[mid] <= y) {
+                    start = mid + 1;
+                } else {
+                    ans = arr.length - mid;
+                    end = mid - 1;
+                }
+            }
+        }
+        if (ans == -1) {
+            return 0;
+        }
+        return ans;
     }
 }
