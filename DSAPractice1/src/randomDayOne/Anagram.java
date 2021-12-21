f3f78eb50cb04a3204def5f2c8d0922d81cdfa54
Ramsay2 <sachin114sharma@gmail.com>
2021-12-21 20:05:38 +0530

    commit

--- DSAPractice1/src/randomDayOne/Anagram.java
+++ DSAPractice1/src/randomDayOne/Anagram.java
@@ -1,17 +1,14 @@
 package randomDayOne;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Anagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- != 0) {
             String s1 = sc.next();
             String s2 = sc.next();
             if (s1.length() != s2.length()) {
-                System.out.println("False");
+                System.out.println("No");
             } else {
                 char[] arr1 = s1.toCharArray();
                 char[] arr2 = s2.toCharArray();
@@ -19,11 +16,11 @@ public class Anagram {
                 Arrays.sort(arr2);
                 if (Arrays.equals(arr1, arr2)) {
 
-                    System.out.println("True");
+                    System.out.println("Yes");
                 } else {
-                    System.out.println("False");
+                    System.out.println("No");
                 }
-            }
+
         }
     }
 }
