e94cb18bd0bfc68a61d7f9569b1ca4b1dad99935
Ramsay2 <sachin114sharma@gmail.com>
2021-12-27 18:51:54 +0530

    commit

---
+++ DSAPractice1/src/randomDayOne/AllTwiceExceptOne.java
@@ -0,0 +1,27 @@
+package randomDayOne;
+
+import java.util.Scanner;
+
+public class AllTwiceExceptOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(findSingle(arr));
+        }
+
+    }
+    static int findSingle(int[] arr) {
+
+        int xor = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            xor = xor ^ arr[i];
+        }
+        return xor;
+    }
+}
